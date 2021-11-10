1669709b0f8d11d1650f5642fc59d2f623c88de8
Jeeva-ctrl <jeevabharathy66@gmail.com>
2021-11-10 14:37:31 +0530

    1 file modified

--- index.js
+++ index.js
@@ -14,9 +14,75 @@ const sum = function (a) {
     }
   };
 };
-
 console.log(typeof sum); // function
 console.log('Recursive Sum', sum(2)(4)(6)());
+
+/*
+ * Polyfill for map
+ */
+
+const arr1 = [1, 2, 3, 4];
+
+Array.prototype.mapPolyfill = function (cb) {
+  let tempArr = [];
+
+  for (let i = 0; i < this.length; i++) {
+    tempArr.push(cb(this[i]));
+  }
+  return tempArr;
+};
+console.log(
+  'map ',
+  arr1.map((x) => x * 2)
+);
+console.log(
+  'map polyfill',
+  arr1.mapPolyfill((x) => x * 2)
+);
+
+/*
+ * Polyfill for filter
+ */
+
+Array.prototype.filterPolyfill = function (cb) {
+  let tempArr = [];
+
+  for (let i = 0; i < this.length; i++) {
+    if (cb(this[i])) {
+      tempArr.push(this[i]);
+    }
+  }
+  return tempArr;
+};
+
+console.log(
+  'filter ',
+  arr1.filter((x) => x > 2)
+);
+console.log(
+  'filter polyfill',
+  arr1.filterPolyfill((x) => x > 2)
+);
+
+/* 
+  Polyfill for bind
+*/
+
+const user = { name: 'John' };
+
+function PrintName() {
+  console.log('My name is ', this.name);
+}
+
+const bindprintName = PrintName.bind(user);
+console.log('bindprintName', bindprintName()); //My name is  John
+
+Function.prototype.bindPolyfill = function (newObj) {
+  return () => this.apply(newObj);
+};
+
+const bindPolyfillprintName = PrintName.bindPolyfill(user);
+console.log('bindPolyfillprintName', bindPolyfillprintName()); //My name is  John
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
